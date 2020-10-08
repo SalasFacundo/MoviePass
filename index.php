7f9b2550024a14bcb5d2ecde82aaef8ce154a03e
Nicoolas juarez <61887365+1Nicolasjuarez@users.noreply.github.com>
2020-10-08 16:29:43 -0300

    prueba

--- index.php
+++ index.php
@@ -3,9 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Movie Pass</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>Movie Pass</h1>
+
+
+
+
+
+     <h2>hola</h2>
+    
 </body>
 </html>
