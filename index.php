7c56aaa803d24ade9509815e73defde81929f7f2
unknown <diego.ullua@outlook.com>
2020-10-08 16:11:00 -0300

    Primer commit, probando git de vuelta

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movie Pass</title>
+</head>
+<body>
+    <h1>Movie Pass</h1>
+</body>
+</html>
