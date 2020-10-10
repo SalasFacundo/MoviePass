dfd7c246784d1a05b902bdec4f87c843d7fd3d5b
Nicoolas juarez <61887365+1Nicolasjuarez@users.noreply.github.com>
2020-10-10 17:20:59 -0300

    v1
    nico

--- Config/Config.php
+++ Config/Config.php
@@ -2,7 +2,7 @@
 
 define("ROOT", dirname(__DIR__) . "/");
 //Path to your project's root folder
-define("FRONT_ROOT", "/Movie");
+define("FRONT_ROOT", "/MoviePass");
 define("VIEWS_PATH", "Views/");
 
 //define("CSS_PATH", FRONT_ROOT . VIEWS_PATH. "css/");
