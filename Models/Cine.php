ff696c9858843eac477d65f3c4edb293beefd968
SalasFacundo <salasfacundo96@gmail.com>
2020-10-10 18:27:23 -0300

    add class Cine.php in Models

---
+++ Models/Cine.php
@@ -0,0 +1,62 @@
+<?php 
+
+namespace Models;
+
+ class Cine 
+ {
+ 	private $id_cine;
+ 	private $nombre_cine;
+ 	private $salas;
+
+ 	function __construct()
+ 	{
+ 		
+ 	}
+
+    
+
+
+    public function getIdCine()
+    {
+        return $this->id_cine;
+    }
+
+    
+    public function setIdCine($id_cine)
+    {
+        $this->id_cine = $id_cine;
+    }
+
+
+
+
+    
+    public function getNombreCine()
+    {
+        return $this->nombre_cine;
+    }
+
+    
+    public function setNombreCine($nombre_cine)
+    {
+        $this->nombre_cine = $nombre_cine;
+    }
+
+
+
+    public function getSalas()
+    {
+        return $this->salas;
+    }
+
+    
+    public function setSalas($salas)
+    {
+        $this->salas = $salas;
+    }
+
+
+} 
+
+
+ ?>
