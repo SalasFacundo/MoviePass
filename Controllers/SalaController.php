b9b45dd1ad565185114d6f70285cbaac018b8161
Diego <diego.ullua@outlook.com>
2020-10-11 23:39:15 -0300

    Agrego los DAO y las clases de Sala, Cine, y Admin listos para agregar al JSON, falta agregar metodos de eliminar y modificar

---
+++ Controllers/SalaController.php
@@ -0,0 +1,37 @@
+<?php 
+
+    namespace Controllers;
+
+    use DAO\SalaDao as SalaDao;
+    use Models\Sala as Sala;
+
+    class SalaController{
+        private $salaDao;
+
+        public function __construct(){
+            $this->salaDao = new SalaDao();
+        }
+        public function showAddView(){
+            require_once(VIEWS_PATH."index.php");
+        }
+
+        public function Add($numeroSala, $capacidad){
+        
+            $salaNueva = new Sala();
+
+            
+            $salaNueva->setNumeroSala($numeroSala);
+            $salaNueva->setCapacidad($capacidad);
+
+            //var_dump($salaNueva);
+            $this->salaDao->add($salaNueva);
+
+            //var_dump($cineNuevo);
+            
+            $this->showAddView();
+        }
+
+    }
+
+
+?>
