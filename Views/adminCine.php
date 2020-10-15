38f0752e0119a3aa078c163b82c99158553f3ec2
Nicoolas juarez <61887365+1Nicolasjuarez@users.noreply.github.com>
2020-10-15 11:39:25 -0300

    Update adminCine.php

--- Views/adminCine.php
+++ Views/adminCine.php
@@ -31,19 +31,67 @@
 
 
 
-            <div class="row" style="background-color: blue;">
+            <div class="row" style="background-color: white;">
 
 
                 <div class="col-sm-12" style="text-align: center;">
-                    <h3>TITULO</h3>
+                    <h3>INGRESAR UN NUEVO CINE</h3>
                 </div>
 
                 <div class="col-sm-4"></div>
-                <div class="col-sm-4" style="text-align: center;">
-                    <h3>fromulario</h3>
-                </div>
-                <div class="col-sm-4"></div>
+                <div class="col-sm-4">
 
+                    <form action="">
+
+                        <div class="form-group ">
+                            <label for="" class="col-sm-4 col-form-label">ID CINE</label>
+                            <div class="col-sm-10">
+                                <input type="number" class="form-control" id="" placeholder="ID cine....">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="" class="col-sm-4 col-form-label">NOMBRE CINE</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="" placeholder="Nombre del cine...">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="" class="col-sm-4 col-form-label">CANTIDAD SALAS</label>
+                            <div class="col-sm-10">
+                                <input type="number" class="form-control" id=""
+                                    placeholder="Numero de total de salas...">
+                            </div>
+                        </div>
+
+
+                        <div class="form-group">
+                            <label for="" class="col-sm-4 col-form-label">DIRECCION</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id=""
+                                    placeholder="Nombre de la calle - numero...">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="" class="col-sm-4 col-form-label">PRECIO ENTRADA</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="" placeholder="$...">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-12">
+                                <button type="submit" class="btn btn-secondary">Aceptar</button>
+                            </div>
+                        </div>
+
+                    </form>
+
+
+
+
+                </div>
 
 
             </div>
@@ -53,38 +101,122 @@
 
 
 
+        </div>
+        <div class="col-sm-4"></div>
 
-            <div class="row" style="background-color: yellow;">
 
-                <div class="col-sm-12" style="text-align: center;">
-                    <h3>moficicar cine</h3>
-                </div>
 
-                <div class="col-sm-4" ></div>
-                <div class="col-sm-4" style="text-align: center;">
-                    <h3>fromulario</h3>
-                </div>
-                <div class="col-sm-4" ></div>
+        </div>
 
 
 
 
-            </div>
 
-            <div class="row" style="background-color: orange;"> 
+
+
+        <div class="row" style="background-color:#ff8a8a;">
+
             <div class="col-sm-12" style="text-align: center;">
-                    <h3>moficicar sala</h3>
-                </div>
+                <h3>moficicar cine</h3>
+            </div>
 
-                <div class="col-sm-4" ></div>
-                <div class="col-sm-4" style="text-align: center;">
-                    <h3>fromulario</h3>
-                </div>
-                <div class="col-sm-4" ></div>
-         
-         
-         
-         </div>
+            <div class="col-sm-4"></div>
+            <div class="col-sm-4" style="text-align: center;">
+                <h3>fromulario</h3>
+            </div>
+            <div class="col-sm-4"></div>
+
+
+
+
+        </div>
+
+        <div class="row" style="background-color:#f01d1d;">
+            <div class="col-sm-12" style="text-align: center;">
+                <h3>ELIMINAR CINE</h3>
+            </div>
+
+            <div class="col-sm-4"></div>
+            <div class="col-sm-4">
+
+                <form action="">
+
+                    <div class="form-group">
+                        <label for="" class="col-sm-4 col-form-label">NUMERO DE ID</label>
+                        <div class="col-sm-10">
+                            <input type="number" class="form-control" id="" placeholder="Numero de id de cine a borrar">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-12">
+                            <button type="submit" class="btn btn-secondary">Aceptar</button>
+                        </div>
+                    </div>
+
+
+
+
+
+                </form>
+
+
+
+
+
+
+            </div>
+            <div class="col-sm-4"></div>
+
+
+
+        </div>
+
+
+        <div class="row">
+            <div class="col-sm-12" style="text-align: center;">
+                <h3>LISTAR CINES</h3>
+            </div>
+
+            <div class="col-sm-4"></div>
+            <div class="col-sm-4" style="text-align: center;">
+
+                <table class="table table-striped table-dark">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Nombre cine</th>
+                            <th scope="col">Cantidad de salas</th>
+                            <th scope="col">Direccion</th>
+                            <th scope="col">Precio de la entrada</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>aldrey</td>
+                            <td>4</td>
+                            <td>sarmiento 1040</td>
+                            <td> $ 150</td>
+                        </tr>
+                        <tr>
+
+
+                    </tbody>
+                </table>
+
+
+
+
+
+
+
+
+            </div>
+            <div class="col-sm-4"></div>
+
+
+
+        </div>
 
 
 
