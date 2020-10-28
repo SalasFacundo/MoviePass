0b0a8839249be93f2bd3c344cd2185c441e84b8e
SalasFacundo <salasfacundo96@gmail.com>
2020-10-28 19:07:30 -0300

    new attributes in funcion.php

--- Models/Funcion.php
+++ Models/Funcion.php
@@ -8,81 +8,106 @@ require_once "../DAO/FuncionDao.php";
 
 class Funcion
 {
-  private $numero_sala;
-  private $id_pelicula=-1;
-  private $hora_inicio;
-  private $hora_final;
+  private $Id_funcion;
+  private $Id_sala;
+  private $Id_pelicula;
+  private $Hora_inicio;
+  private $Hora_fin;
+  private $Dia;
+  private $Activo;
 
   function __construct()
   {
 
   }
-
-
-
-  public function getSala()
-  {
-    return $this->numero_sala;
-}
-
-
-public function setSala($numero_sala)
-{
-    $this->numero_sala = $numero_sala;
-}
-
-
-public function getIdPelicula()
-{
-    return $this->id_pelicula;
-}
-
-
-public function setIdPelicula($id_pelicula)
-{
-    $this->id_pelicula = $id_pelicula;
-}
-
-
-public function getHoraInicio()
-{
-    return $this->hora_inicio;
-}
-
-
-public function setHoraInicio($hora_inicio)
-{
-
-    $this->hora_inicio = $hora_inicio;
-
-    $pelicula = file_get_contents(
-        "http://api.themoviedb.org/3/movie/".$this->id_pelicula."?api_key=a813ce03ea202b120e2307c4325bd6c3&language=en-US");
-
-    $duracion = json_decode($pelicula, true)["runtime"];    
-
-
-
-    $h = explode(':',$hora_inicio);     
-    $this->hora_final= date("H:i", mktime($h[0],$h[1]+$duracion+15));
+    
+    public function getIdFuncion()
+    {
+        return $this->Id_funcion;
+    }
 
     
+    public function setIdFuncion($Id_funcion)
+    {
+        $this->Id_funcion = $Id_funcion;
+    }
 
+    
+    public function getIdSala()
+    {
+        return $this->Id_sala;
+    }
+
+    
+    public function setIdSala($Id_sala)
+    {
+        $this->Id_sala = $Id_sala;
+    }
+
+    
+    public function getIdPelicula()
+    {
+        return $this->Id_pelicula;
+    }
+
+    
+    public function setIdPelicula($Id_pelicula)
+    {
+        $this->Id_pelicula = $Id_pelicula;
+    }
+
+    
+    public function getHoraInicio()
+    {
+        return $this->Hora_inicio;
+    }
+
+    
+    public function setHoraInicio($Hora_inicio)
+    {
+        $this->Hora_inicio = $Hora_inicio;
+    }
+
+    
+    public function getHoraFin()
+    {
+        return $this->Hora_fin;
+    }
+
+    
+    public function setHoraFin($Hora_fin)
+    {
+        $this->Hora_fin = $Hora_fin;
+    }
+
+    
+    public function getDia()
+    {
+        return $this->Dia;
+    }
+
+    
+    public function setDia($Dia)
+    {
+        $this->Dia = $Dia;
+    }
+
+    
+    public function getActivo()
+    {
+        return $this->Activo;
+    }
+
+    
+    public function setActivo($Activo)
+    {
+        $this->Activo = $Activo;
+    }
 }
 
-public function getHoraFinal()
-{
-    return $this->hora_final;
-}
 
 
-public function setHora($hora_final)
-{
-    $this->hora_final = $hora_final;
-}
-
-
-
-}
+ 
 
 
 ?>
